df753d8d46981b653c436d1dc5baf7da6781c7c1
daya <583413880@qq.com>
2021-07-02 20:33:44 +0800

    配置提交

--- CryBt/packConfig/minigame/CfgPackage.js
+++ CryBt/packConfig/minigame/CfgPackage.js
@@ -1,8 +1,8 @@
 (function() {
     window.LOG_LEVEL = 0;
     window.APP_VERSION = [1, 0, 0];
-    window.LOGIN_SERVER = "http://47.96.128.102:8123/CryBt/";
-    window.BACKUP_LOGIN_SERVER = "http://47.96.128.102:8123/CryBt/";
+    window.LOGIN_SERVER = "https://jingouyar.cn/CryBt/";
+    window.BACKUP_LOGIN_SERVER = "https://jingouyar.cn/CryBt/";
     window.CHANNEL_EXT = {
         wxappid: "wx4addc9c696a582a8",
     };
